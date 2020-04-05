ae939814b951585019a2a44042918d236c8a66e3
Kummita Sriteja <sriteja.ku@gmail.com>
2020-04-05 23:46:12 +0200

    Update dynamicCG.py

--- dynamicCG.py
+++ dynamicCG.py
@@ -1,6 +1,13 @@
+import sys
 import pytest
 import py_call_graph
 
 
 if __name__ == '__main__':
-    pytest.main(['-x', './tests/'], plugins=[py_call_graph])
+    if len(sys.argv) > 1:
+        tc_file = sys.argv[1]
+        with open(tc_file) as f:
+            for line in f:
+                pytest.main(['-x', str(line.replace("\n", ""))], plugins=[py_call_graph])
+    else:
+        pytest.main(['-x', './tests/'], plugins=[py_call_graph])
